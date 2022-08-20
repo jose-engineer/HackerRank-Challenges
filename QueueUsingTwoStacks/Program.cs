78d7fc40d66c6023a549819c23abeed52dfc2592
jose-engineer <jose-engineer@outlook.com>
2022-08-20 18:18:56 -0500

    Clean solution

--- QueueUsingTwoStacks/Program.cs
+++ QueueUsingTwoStacks/Program.cs
@@ -2,101 +2,43 @@
 using System.Collections.Generic;
 
 namespace QueueUsingTwoStacks {
+    //https://www.hackerrank.com/challenges/queue-using-two-stacks/problem
+
     class Program {
         static void Main(string[] args) {
-            var numberOfQueries = int.Parse(Console.ReadLine());
-            var stack1 = new Stack<int>();
-            var stack2 = new Stack<int>();
-            var queueFrontStack1 = false;
 
-            for (int i = 0; i < numberOfQueries; i++) {
-                var inputSplits = Console.ReadLine().Split(' ');
-                var queryType = int.Parse(inputSplits[0]);
-                switch (queryType) {
-                    case 1:
-                        if (stack1.Count == 0 && stack2.Count == 0) {
-                            stack1.Push(int.Parse(inputSplits[1]));
-                            queueFrontStack1 = true;
-                        } else
-                            stack2.Push(int.Parse(inputSplits[1]));
+            int queries = int.Parse(Console.ReadLine());
+            Stack<int> input = new Stack<int>();
+            Stack<int> output = new Stack<int>();
 
-                        break;
-                    case 2:
-                        if (queueFrontStack1 == true) {
-                            stack1.Pop();
-                            if (stack2.Count != 0 && stack1.Count == 0)
-                                queueFrontStack1 = false;
-                        } else {
-                            if (stack2.Count > 0) {
-                                while (stack2.Count > 1)
-                                    stack1.Push(stack2.Pop());
+            while (queries > 0) {
+                string[] splits = Console.ReadLine().Split(' ');
+                int type = int.Parse(splits[0]);
+                //int value = int.Parse(splits[1]);
 
-                                stack2.Pop();
-                                queueFrontStack1 = true;
-                            }
-
-                        }
-                        break;
-                    case 3:
-                        if (queueFrontStack1 == true)
-                            Console.WriteLine(stack1.Peek());
-                        else {
-                            if (stack2.Count > 0) {
-                                while (stack2.Count > 0)
-                                    stack1.Push(stack2.Pop());
-
-                                queueFrontStack1 = true;
-                                Console.WriteLine(stack1.Peek());
-                            }
-
-                        }
-                        break;
+                if (type == 1) {
+                    input.Push(int.Parse(splits[1]));
+                } else if (type == 2) {
+                    if (output.Count == 0) {
+                        shiftStacks(input, output);
+                    }
+                    output.Pop();
+                } else if (type == 3) {
+                    if (output.Count == 0) {
+                        shiftStacks(input, output);
+                    }
+                    Console.WriteLine(output.Peek());
                 }
+
+                queries--;
             }
+
         }
 
-        static void Main2(string[] args) {
-            int inp = Convert.ToInt32(Console.ReadLine());
-
-            Stack<int> front = new Stack<int>();
-            Stack<int> rear = new Stack<int>();
-            for (int i = 1; i <= inp; i++) {
-
-
-                string q = Console.ReadLine();
-                string[] n = q.Split(' ');
-                int x = Convert.ToInt32(n[0]);
-                int y = 0;
-                if (n.Length > 1) {
-                    y = Convert.ToInt32(n[1]);
-                }
-                if (x == 1) {
-
-                    rear.Push(y);
-                } else {
-
-                    if (front.Count == 0) {
-                        while (rear.Count > 0) {
-                            front.Push(rear.Peek());
-                            rear.Pop();
-                        }
-                        if (x == 2) {
-
-                            front.Pop();
-                        } else if (x == 3) {
-                            Console.WriteLine(front.Peek());
-                        }
-                    } else {
-                        if (x == 2) {
-
-                            front.Pop();
-                        } else if (x == 3) {
-                            Console.WriteLine(front.Peek());
-                        }
-
-                    }
-
-                }
+        public static void shiftStacks(Stack<int> input, Stack<int> output) {
+            while (input.Count != 0) {
+                output.Push(input.Peek()); //output.Push(input.Pop());                
+                input.Pop();
             }
         }
     }
