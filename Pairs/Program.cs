62a48187bc86f07546e05fdcf7ac53646e23d548
jose-engineer <jose-engineer@outlook.com>
2022-08-21 17:13:46 -0500

    Challenge description

---
+++ Pairs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Pairs {
+    //    Given an array of integers and a target value, determine the number of pairs of array elements that have a difference equal to the
+    //    target value.
+
+    //Example
+    //k=1
+    //arr=[1,2,3,4]
+    //There are three values that differ by k=1 : 2-1 = 1, 3-2 = 1, and 4-3 = 1. Return 3.
+
+    //Function Description
+    //Complete the pairs function below.
+    //pairs has the following parameter(s):
+
+    //int k: an integer, the target difference
+    //int arr[n]: an array of integers
+
+    //Returns
+    //int: the number of pairs that satisfy the criterion
+
+//Sample Input
+//STDIN Function
+//-----       --------
+//5 2         arr[] size n = 5, k =2
+//1 5 3 4 2   arr = [1, 5, 3, 4, 2]
+
+//Sample Output
+//3
+
+//Explanation
+//There are 3 pairs of integers in the set with a difference of 2: [5,3], [4,2] and[3, 1]. .
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
